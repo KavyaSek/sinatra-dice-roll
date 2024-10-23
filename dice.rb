c07affb5a014c473c173ef5408fd57b4724ca2b6
KavyaSek <ksekar@chicagobooth.edu>
2024-10-23 02:36:49 +0000

    AA

--- dice.rb
+++ dice.rb
@@ -1,62 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/") do
-  erb(:elephant)  
+get("/")do
+erb(:home_page)
 end
 
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾, does it?"
-end
-
-get ("/dice/2/6") do
+get("/dice/:number_of_dice/:number_of_sides") do
   @rolls = []
-  2.times do
-    die = rand (1..6)
-    @rolls.push(die)
-  end
-  
-  erb(:two_six)	
-end 
+  @num_dice = params.fetch("number_of_dice").to_i
+  @num_sides = params.fetch("number_of_sides").to_i
 
-get ("/dice/2/10") do
-  @rolls = []
-  2.times do
-    die = rand (1..10)
-    @rolls.push(die)
-  end
-  
-  erb(:two_ten)
-
-end 
-
-get ("/dice/1/20") do
-  @rolls = []
-  1.times do
-    die = rand (1..20)
-    @rolls.push(die)
-  end
-  	
-  erb(:one_twenty)
-end 
-
-get ("/dice/5/4") do
-  @rolls = []
-  5.times do
-    die = rand (1..4)
+  @num_dice.times do
+    die = rand(1..@num_sides)
     @rolls.push(die)
   end
 
-  erb(:five_four)
-  
-end 
-
-get ("/dice/100/6") do
-  @rolls = []
-  100.times do
-    die = rand (1..6)
-    @rolls.push(die)
-  end
-
-erb(:hundred_six)
+  erb(:flexible)
 end
